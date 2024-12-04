ed1cd0e4cac5dba1d6fc59c990530940a0ea20c5
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-04 16:21:05 +0100

    feat: esercizio base fatto. inmplemento controllers e sposto i crug (WIP)

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+// richiamo le route
+const postsRouters = require("./routers/crug");
+
 // creo route di base
 app.get("/", (req, res)=>{
     console.log("sono route di base");
--- routers/crug.js
+++ routers/crug.js
@@ -1,3 +1,38 @@
 const express = require("express");
 const router = express.Router();
+// importo dati da lista dati
+const bacheca = require("../data/data");
 
+// index
+router.get("/", (req, res)=>{
+    res.json(bacheca);
+});
+
+// show
+router.get("/:id", (req,res)=>{
+    const curId = req.params.id;
+    res.json("vedo id specifico" + curId);
+
+});
+// create
+router.post("/", (req,res)=>{
+    res.json("creo");
+})
+// modify
+router.put("/:id",(req,res)=>{
+    const curId = req.params.id;
+    res.json("qui modifico tutto l'elemento dell' id" + curId);
+})
+
+// modify dettail
+router.patch("/:id",(req, res)=> {
+    const curId = req.params.id;
+    res.json("qui modifico solo un parte dell'elemento" + curId);
+})
+// delete
+router.delete("/:id",(req,res) => {
+const curId = req.params.id;
+req.json("qui elimino elemenco con id" + curId);
+});
+
+module.exports = router;
